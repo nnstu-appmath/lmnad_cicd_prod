075547debde496f7a3717518f2264cb59899e6d5
Peshina Anastasiya <62890415+PeshinaA@users.noreply.github.com>
2022-11-07 23:33:00 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,6 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -qa)
 #cd lmnad
 #docker-compose -f docker-compose.dev.yml up -d --build
-rm -rf lmnad
+ls -l
 #docker ps
 #docker logs lmnad_web_dev
