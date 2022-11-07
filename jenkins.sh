01ee076afe2a5b65a4d9c5e38df77a201795c521
Peshina Anastasiya <62890415+PeshinaA@users.noreply.github.com>
2022-11-07 23:08:36 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+docker-compose down --remove-orphans
+docker system prune
 git clone https://github.com/nnstu-appmath/lmnad.git
 cd lmnad
+docker-compose pull
 docker-compose up -d --build
 docker ps
 docker logs lmnad_web
