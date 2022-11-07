e571bcf0458bc55ee993d39f52b8aa4aa7fccd3a
Peshina Anastasiya <62890415+PeshinaA@users.noreply.github.com>
2022-11-07 21:17:22 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#git clone https://github.com/nnstu-appmath/lmnad.git
-docker run hello-world
-#cd lmnad
-#docker-compose up -d --build
+git clone https://github.com/nnstu-appmath/lmnad.git
+cd lmnad
+docker-compose up -d --build
