359090febd4895311d23db863e8b4fa799fce391
Peshina Anastasiya <62890415+PeshinaA@users.noreply.github.com>
2022-11-11 21:29:19 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #docker stop $(docker ps -a -q)
 #docker rm $(docker ps -qa)
-docker-compose -f lmnad/docker-compose.dev.yml up -d --build
+docker-compose -f docker-compose.dev.yml up -d --build
 #ls -l
 #docker exec lmnad_web_dev sh -c "python manage.py migrate"
 docker ps
